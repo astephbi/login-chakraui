280f4c342e1b9446339317a5a46049cabe192608
Alex <astephbi@gmail.com>
2022-10-08 14:08:15 +0000

    login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Flex, Heading, Button, Input, useColorMode, useColorModeValue } from '@chakra-ui/react'
 
 function App() {
+  const name = 'Alex';
+  const {toggleColorMode} = useColorMode();
+  const formBackground = useColorModeValue("gray.100")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex direction="column" p={12} rounded={6} background={formBackground}>
+        <Heading mb={6}>Login</Heading>
+        <Input placeholder='astephbi@gmail.com' variant="filled" mb={3} type="email"/>
+        <Input placeholder='************' variant="filled" mb={6} type='password'/>
+        <Button mb={6} colorScheme="teal">Login</Button>
+        <Button onClick={toggleColorMode}> Toggle Color Mode </Button>
+
+
+      </Flex>
+    </Flex>
   );
 }
 
